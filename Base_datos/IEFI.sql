f96930f5afdf54b37d8f514c2cd61ddf7a69885f
mario <mario.gonzalez@hotmail.com.ar>
2021-11-12 21:48:59 -0300

    a2

---
+++ Base_datos/IEFI.sql
@@ -0,0 +1,12 @@
+/*-------------------------------------------------------*/
+/*Paso 01 Borra cualquier base de datos con ese nombre  */
+/*-------------------------------------------------------*/
+DROP DATABASE IF EXISTS iefi_programacion_1;
+/*-------------------------------------------------------*/
+/*Paso 02 Crea una base de datos con ese nombre         */
+/*-------------------------------------------------------*/
+CREATE DATABASE iefi_programacion_1;
+/*-------------------------------------------------------*/
+/*Paso 03 Seleciona una base de datos con ese nombre     */
+/*-------------------------------------------------------*/
+USE iefi_programacion_1;
