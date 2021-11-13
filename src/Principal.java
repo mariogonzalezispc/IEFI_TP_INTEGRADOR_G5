201159f40bda385d03f7c3932759f4b256e90394
mario <mario.gonzalez@hotmail.com.ar>
2021-11-13 12:00:49 -0300

    prin

--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,45 @@
 public class Principal {
     public static void main(String[] args) throws Exception {
         System.out.println("Hola Mundo!");
+        int accion=5;
+
+
+
+
+
+
+
+
+
+		switch(accion) {
+			case 1:
+				System.out.println("Habitación 1");
+                break;
+			case 2:
+				System.out.println("Habitación 2");
+                break;
+            case 3:
+				System.out.println("Habitación 3");
+                break;
+                case 4:
+				System.out.println("Habitación 4");
+                break;
+                case 5:
+				System.out.println("Habitación 5");
+                break;
+                case 6:
+				System.out.println("Habitación 6");
+                break;
+                case 7:
+				System.out.println("Habitación 7");
+                break;
+                case 8:
+				System.out.println("Habitación 8");
+                break;
+                default:
+				System.out.println("Defecto");
+		}
+
+
     }
 }
