0783e91604cab94f88ecf44871b255cd2b98431f
Mario <mario.gonzalez@hotmail.com.ar>
2021-11-17 10:48:54 -0300

    java

--- src/IEFI/Conector/Conectar1.java
+++
@@ -1,9 +0,0 @@
-package IEFI.Conector;
-//import java.sql.*;
-public class Conectar1 {
-
-    public static void main(String[] args) {
-        //Connection conexion = null;
-
-    }
-}
--- src/IEFI/Principal/Principal.java
+++ src/IEFI/Principal/Principal.java
@@ -1,10 +1,20 @@
 package IEFI.Principal;
 //import java.sql.*;
-//import Conector.Conectar_bd;
+import IEFI.Conector.*;
 
+//import Conector.Conectar_bd;
 
 public class Principal {
     public static void main(String[] args) throws Exception {
+        Conectar_bd C1=new Conectar_bd();
+        C1.setConecto("15");
+
+
+
+        
+        System.out.println("carga direccion  "+C1.getConecto());
+
+
         System.out.println("Hello, World!");
     }
 }
