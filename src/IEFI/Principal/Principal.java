11286170117731166b257de4573595b29a460166
Mario <mario.gonzalez@hotmail.com.ar>
2021-11-17 10:23:16 -0300

    java

---
+++ src/IEFI/Principal/Principal.java
@@ -0,0 +1,10 @@
+package Principal;
+//import java.sql.*;
+//import Conector.Conectar_bd;
+
+
+public class Principal {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
