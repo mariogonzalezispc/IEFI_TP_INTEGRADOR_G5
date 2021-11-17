93c01b011b42ed3c293d0650bb668a0aea47bdde
Mario <mario.gonzalez@hotmail.com.ar>
2021-11-17 12:29:19 -0300

    java

--- src/IEFI/Principal/Principal.java
+++ src/IEFI/Principal/Principal.java
@@ -13,8 +13,6 @@ public class Principal {
         C1.setUrl("jdbc:mysql://localhost:3306/iefi_programacion_1");//url de la base de datos
         C1.setUsuario("root");//usuario de la base de datos
         C1.setClave("1234");//clave de la base de datos
-        
-   
      //---------------------------------------------------------------------- 
         try {
             boolean salir=false;//declaro variable boleana para el while
@@ -31,56 +29,57 @@ public class Principal {
      //----------------------------------------------------------------------
      //Switch para decidir el valor ingresado por el usuario
      //----------------------------------------------------------------------
-            switch(accion) {
+            switch(accion) 
+                {
      //---------------------------------------------------------------------- 
                     case 1://generar una conexion
-                    System.out.println("Conexion : "+C1.getConecto());
-                    Thread.sleep(2000);
-                    //   System.out.println("Conectandose"); 
-                    //   System.out.println("");          
-                    //   st=cn.con.createStatement();//esto genera la conexion con la BD IEFI_programacion_1
-                    //   System.out.println(""); 
-                    System.out.println("Opcion 1"); 
+                        System.out.println("Conexion : "+C1.getConecto());
+                        Thread.sleep(2000);
+                        //   System.out.println("Conectandose"); 
+                        //   System.out.println("");          
+                        //   st=cn.con.createStatement();//esto genera la conexion con la BD IEFI_programacion_1
+                        //   System.out.println(""); 
+                        System.out.println("Opcion 1"); 
                     break;
      //---------------------------------------------------------------------- 
                     case 2:
-                    System.out.println("Opcion 2"); 
+                        System.out.println("Opcion 2"); 
                     break;
      //---------------------------------------------------------------------- 
                     case 3:
-                    System.out.println("Opcion 3");             
+                        System.out.println("Opcion 3");             
                     break;
      //----------------------------------------------------------------------              
                     case 4:
-                    System.out.println("Opcion 4"); 
+                        System.out.println("Opcion 4"); 
                     break;
      //-----------------------------------------------------------------------
                     case 5:
-                    System.out.println("Opcion 5");
+                        System.out.println("Opcion 5");
                     break;
      //-------------------------------------------------------------------------
                     case 6:
-                    System.out.println("Opcion 6");
+                        System.out.println("Opcion 6");
                     break;
      //---------------------------------------------------------------------- 
                     case 7:
-                    System.out.println("Opcion 7");
+                        System.out.println("Opcion 7");
                     break;
      //---------------------------------------------------------------------- 
                     case 8:
-                    System.out.println("Opcion 8");
+                        System.out.println("Opcion 8");
                     break;
      //---------------------------------------------------------------------- 
                     case 9://sale del programa
-                    System.out.println("Fin de programa");
-                    opcion.close();//cierra scanner
-                    salir=true;// sale del while
+                        System.out.println("Fin de programa");
+                        opcion.close();//cierra scanner
+                        salir=true;// sale del while
                     break;
      //----------------------------------------------------------------------                 
                     default://cae a default solo si es reiniciado por la exeption 
-                    System.out.println("Reintente ingresar opcion");
+                        System.out.println("Reintente ingresar opcion");
                     break;                    
-            }
+                }
         }
          } catch (Exception e) {
             System.out.println(e);
